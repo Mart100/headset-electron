3457d667fb9e8d3b729ddf6042de5a6f61b1d1b3
Felipe Castillo <fcastillo.ec@gmail.com>
2018-12-11 22:51:38 -0800

    Fix using organization variable rather than headsetapp

--- bin/cask_version_bump.sh
+++ bin/cask_version_bump.sh
@@ -14,7 +14,7 @@ readonly submit_pr_to='homebrew:master'
 readonly cask_branch='cask_repair_update-headset'
 readonly caskroom_taps_dir="$(brew --repository)/Library/Taps/homebrew"
 readonly organization='headsetapp'
-readonly submit_pr_from="${headsetapp}:${cask_branch}"
+readonly submit_pr_from="${organization}:${cask_branch}"
 readonly installer_path="${TRAVIS_BUILD_DIR}/darwin/build/installers"
 readonly installer_file="${installer_path}/$(ls ${installer_path} | grep dmg)"
 readonly cask_version=${TRAVIS_TAG:1}
@@ -23,15 +23,12 @@ readonly pr_message="${commit_message}\n\nAfter making all changes to the cask:\
 readonly submission_error_log="$(mktemp)"
 readonly divide=$(hr -)
 
-# Function for color output, first argument is color, second is the message
-function color_message { echo -e "$(tput setaf "${1}")${2}$(tput sgr0)"; }
-
 cd "${caskroom_taps_dir}"/homebrew-cask/Casks || exit 1
 
 # Checks the headset remote is listed
-if ! git remote | grep --silent "${headsetapp}"; then
-  color_message "yellow" "A \`${headsetapp}\` remote does not exist. Creating it now…"
-  hub fork --org="${headsetapp}"
+if ! git remote | grep --silent "${organization}"; then
+  echo -e "A \`${organization}\` remote does not exist. Creating it now…"
+  hub fork --org="${organization}"
 fi
 
 # Create branch or checkout if it already exists
@@ -39,7 +36,7 @@ git rev-parse --verify "${cask_branch}" &>/dev/null && git checkout "${cask_bran
 
 # Prints the current cask file
 echo -e "\n${divide}"
-color_message "cyan" "Current Headset cask file:"
+echo "Current Headset cask file:"
 echo "${divide}"
 cat "${cask_file}"
 echo "${divide}"
@@ -53,12 +50,12 @@ sed -i.bak "s|sha256 .*|sha256 '${package_sha}'|" "${cask_file}"
 rm "${cask_file}.bak"
 
 echo -e "\n${divide}"
-color_message "bold" "$(git --no-pager diff)" # Displays the difference between files
+git --no-pager diff # Displays the difference between files
 echo "${divide}"
 
 # Error if no changes were made, submit otherwise
 if git diff-index --quiet HEAD --; then
-  color_message "red" 'No changes made to the cask. Exiting...'
+  echo 'No changes made to the cask. Exiting...'
   exit 2
 else
   echo 'Submitting…'
@@ -66,21 +63,21 @@ fi
 
 # Commits and pushes
 git commit "${cask_file}" --message "${commit_message}"
-git push --force "${headsetapp}" "${cask_branch}" 2> "${submission_error_log}"
+git push --force "${organization}" "${cask_branch}" 2> "${submission_error_log}"
 
 # Checks if 'git push' had any errors and attempts to fix "shallow update" error
 if [[ "${?}" -ne 0 ]]; then
   if grep --quiet 'shallow update not allowed' "${submission_error_log}"; then
-    color_message "yellow" 'Push failed due to shallow repo. Unshallowing…'
+    echo 'Push failed due to shallow repo. Unshallowing…'
     HOMEBREW_NO_AUTO_UPDATE=1 brew tap --full "homebrew/$(basename $(git remote get-url origin) '.git')"
-    git push --force "${headsetapp}" "${cask_branch}" 2> "${submission_error_log}"
+    git push --force "${organization}" "${cask_branch}" 2> "${submission_error_log}"
 
     if [[ "${?}" -ne 0 ]]; then
-      color_message "red" "'There were errors while pushing:'\n$(< "${submission_error_log}")"
+      echo -e "'There were errors while pushing:'\n$(< "${submission_error_log}")"
       exit 3
     fi
   else
-    color_message "red" "'There were errors while pushing:'\n$(< "${submission_error_log}")"
+    echo -e "'There were errors while pushing:'\n$(< "${submission_error_log}")"
     exit 3
   fi
 fi
@@ -89,8 +86,8 @@ fi
 pr_link=$(hub pull-request -b "${submit_pr_to}" -h "${submit_pr_from}" -m "$(echo -e "${pr_message}")")
 
 if [[ -n "${pr_link}" ]]; then
-  color_message "green" "\nSubmitted (${pr_link})\n"
+  echo -e "\nSubmitted (${pr_link})\n"
 else
-  color_message "red" 'There was an error submitting the pull request. Please open a bug report on the repo for this script (https://github.com/vitorgalvao/tiny-scripts).'
+  echo -e 'There was an error submitting the pull request. Please open a bug report on the repo for this script (https://github.com/vitorgalvao/tiny-scripts).'
   exit 4
 fi
